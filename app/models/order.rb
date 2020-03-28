38fc90fac0ba0b8d7a6cd601848a62fde4aab54f
drakedalfa <drakedalfa@gmail.com>
2020-03-28 13:49:27 -0600

    ...
    ...

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,12 +1,14 @@
+# frozen_string_literal: true
+
 class Order < ApplicationRecord
   has_many :line_items, dependent: :destroy
   belongs_to :user
 
   def sub_total
     sum = 0
-    self.line_items.each do |line_item|
-      sum+= line_item.total_price
+    line_items.each do |line_item|
+      sum += line_item.total_price
     end
-    return sum
+    sum
   end
 end
